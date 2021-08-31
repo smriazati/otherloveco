ac22e1669ce736c01b03b0008f9b1081d9b9d9f2
= <=>
2021-08-30 20:12:15 -0400

    sanity test

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,8 +53,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: ['@nuxtjs/sanity/module'],
+
+
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
